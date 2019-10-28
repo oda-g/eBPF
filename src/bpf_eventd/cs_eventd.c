7a580959fed3d1ded53ee06bcc3d7c1460ae2efb
Itsuro Oda <oda@valinux.co.jp>
2019-10-28 10:15:12 +0900

    add perf event example

--- src/bpf_eventd/cs_eventd.c
+++ src/bpf_eventd/cs_eventd.c
@@ -1,7 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-#include "bpf_defs.h"
+#define _GNU_SOURCE
+#include <unistd.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <sys/sysinfo.h>
+#include <sys/syscall.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <signal.h>
@@ -9,13 +13,9 @@
 #include <linux/perf_event.h>
 #include <errno.h>
 #include <string.h>
-#include "bpf_eventd.h"
 
-#define MAX_CPU 32
 #define PAGE_SIZE 4096
 #define NR_DATA_PAGE 4 /* must be power of 2 */
-
-#define MAP_PATH BPF_FS MAP_NAME
 
 static void
 handle_signal(int sig)
@@ -25,8 +25,8 @@ handle_signal(int sig)
 
 struct perf_event_sample {
         struct perf_event_header header;
-        uint32_t size;
-	char data[];
+        uint32_t pid, tid;
+        uint64_t time;
 };
 
 struct perf_event_lost {
@@ -74,9 +74,8 @@ output_event(void *map_base)
 
                 if (e_hdr->type == PERF_RECORD_SAMPLE) {
 			struct perf_event_sample *e = (void *)e_hdr;
-			struct bpf_sys_data *d = (void *)e->data;
-			printf("cmd: %u, cpu: %u, time: %lu\n",
-				d->cmd, d->cpu, d->time);	
+			printf("pid: %u, tid: %u, time: %lu\n",
+				e->pid, e->tid, e->time);	
                 } else if (e_hdr->type == PERF_RECORD_LOST) {
 			struct perf_event_lost *lost = (void *)e_hdr;
                         fprintf(stderr, "lost %lu events\n", lost->lost);
@@ -91,118 +90,58 @@ output_event(void *map_base)
         header->data_tail = data_head;
 }
 
-static int map_open(int nr_cpu)
-{
-	char *map_path = MAP_PATH;
-	char *map_name = MAP_NAME;
-	union bpf_attr attr;
-	int fd;
-
-	bzero(&attr, sizeof(attr));
-	attr.pathname = (unsigned long)map_path;
-	fd = bpf_sys(BPF_OBJ_GET, &attr);
-	if (fd >= 0) {
-		fprintf(stderr, "Warning: Map exists.\n");
-		return fd;
-	}
-
-	bzero(&attr, sizeof(attr));
-	attr.map_type = BPF_MAP_TYPE_PERF_EVENT_ARRAY;
-	attr.key_size = 4;
-	attr.value_size = 4;
-	attr.max_entries = nr_cpu;
-	strncpy(attr.map_name, map_name, BPF_OBJ_NAME_LEN - 1);
-	fd = bpf_sys(BPF_MAP_CREATE, &attr);
-	if (fd < 0) {
-		perror("map create failed.");
-		return -1;
-	}
-
-	bzero(&attr, sizeof(attr));
-	attr.pathname = (unsigned long)map_path;
-	attr.bpf_fd = fd;
-	if (bpf_sys(BPF_OBJ_PIN, &attr) == -1) {
-		perror("map pin failed.");
-		close(fd);
-		return -1;
-	}
-
-	return fd;
-}
-
 int main(int argc, char *argv[])
 {
 	int nr_cpu;
-	int map_fd;
-	struct pollfd pfd[MAX_CPU];
+	int cpu;
+	char *endp;
+	int fd;
+	struct pollfd pfd;
+	void *perf_header;
 	struct perf_event_attr ev_attr;
-	void *perf_header[MAX_CPU];
-	int i;
 
-	nr_cpu = get_nprocs_conf();
-	if (nr_cpu < 1 || nr_cpu > MAX_CPU) {
-		fprintf(stderr, "Invalid cpu number: %d\n", nr_cpu);
+	if (argc != 2) {
+		fprintf(stderr, "usage: cs_eventd CPU\n");
 		return 1;
 	}
-	fprintf(stderr, "number of cpu: %d\n", nr_cpu);
+
+	nr_cpu = get_nprocs_conf();
+
+	cpu = strtol(argv[1], &endp, 10);
+	if ('\0' != *endp || cpu >= nr_cpu) {
+		fprintf(stderr, "invalid cpu number: %s\n", argv[1]);
+		return 1;
+	}
 
 	/* NOTE: This function will return without freeing resources
-	 * (open files, mapping memory).
+	 * (open files, mapping memory). 
 	 * They are expected to free at the process exit.
 	 */
 
-	map_fd = map_open(nr_cpu);
-	if (map_fd < 0) {
+	bzero(&ev_attr, sizeof(ev_attr));
+	ev_attr.sample_type = PERF_SAMPLE_TID | PERF_SAMPLE_TIME;
+	ev_attr.type = PERF_TYPE_SOFTWARE;
+	ev_attr.config = PERF_COUNT_SW_CONTEXT_SWITCHES;
+	ev_attr.sample_period = 1;
+	ev_attr.wakeup_events = 1;
+
+	fd = syscall(__NR_perf_event_open, &ev_attr, -1, cpu, -1,
+			PERF_FLAG_FD_CLOEXEC);
+	if (fd < 0) {
+		fprintf(stderr, "perf_event_open falied");
 		return 1;
 	}
 
-	bzero(pfd, sizeof(pfd));
-	bzero(&ev_attr, sizeof(ev_attr));
-	ev_attr.sample_type = PERF_SAMPLE_RAW;
-	ev_attr.type = PERF_TYPE_SOFTWARE;
-	ev_attr.config = PERF_COUNT_SW_BPF_OUTPUT;
-	ev_attr.sample_period = 1;
-	ev_attr.wakeup_events = 1;
-	for (i = 0; i < nr_cpu; i++) {
-		int fd;
-		union bpf_attr attr;
-		void *base;
+	if (ioctl(fd, PERF_EVENT_IOC_ENABLE, 0) < 0) {
+		fprintf(stderr, "event enable falied");
+		return 1;
+	}
 
-		fd = syscall(__NR_perf_event_open, &ev_attr, -1, i, -1,
-				PERF_FLAG_FD_CLOEXEC);
-		if (fd < 0) {
-			fprintf(stderr, "perf_event_open(%d) falied: %s\n",
-				       i, strerror(errno));
-			return 1;
-		}
-
-		bzero(&attr, sizeof(attr));
-		attr.map_fd = map_fd;
-		attr.key = (unsigned long)&i;
-		attr.value = (unsigned long)&fd;
-		if (bpf_sys(BPF_MAP_UPDATE_ELEM, &attr) < 0) {
-			fprintf(stderr, "update map(%d) falied: %s\n",
-				       i, strerror(errno));
-			return 1;
-		}
-
-		if (ioctl(fd, PERF_EVENT_IOC_ENABLE, 0) < 0) {
-			fprintf(stderr, "event enable(%d) falied: %s\n",
-				       i, strerror(errno));
-			return 1;
-		}
-
-		base = mmap(NULL, PAGE_SIZE * (NR_DATA_PAGE + 1),
-			       	PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-		if (base == MAP_FAILED) {
-			fprintf(stderr, "mmap(%d) falied: %s\n",
-				       i, strerror(errno));
-			return 1;
-		}
-
-		pfd[i].fd = fd;
-		pfd[i].events = POLLIN;
-		perf_header[i] = base;
+	perf_header = mmap(NULL, PAGE_SIZE * (NR_DATA_PAGE + 1),
+		       	PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (perf_header == MAP_FAILED) {
+		fprintf(stderr, "mmap falied");
+		return 1;
 	}
 
 	if (signal(SIGINT, handle_signal) == SIG_ERR) {
@@ -211,19 +150,20 @@ int main(int argc, char *argv[])
 	}
 
 	fprintf(stderr, "Running.\n");
+
+	pfd.fd = fd;
+	pfd.events = POLLIN;
 	for (;;) {
-		if (poll(pfd, nr_cpu, -1) == -1) {
+		if (poll(&pfd, 1, -1) == -1) {
 			perror("poll");
 			return 1;
 		}
-		for (i = 0; i < nr_cpu; i++) {
-			if (pfd[i].revents | POLLIN) {
-				output_event(perf_header[i]);
-			} else if (pfd[i].revents != 0) {
-				fprintf(stderr, "poll(%d) error revents(%d)\n",
-						i, pfd[i].revents);
-				return 1;
-			}
+		if (pfd.revents | POLLIN) {
+			output_event(perf_header);
+		} else if (pfd.revents != 0) {
+			fprintf(stderr, "poll error revents(%d)\n",
+					pfd.revents);
+			return 1;
 		}
 	}
 
